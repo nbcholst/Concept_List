a1bb8c2b2dd40fc37013102a957bdffb7870dfd1
nbcholst <nbcholst@gmail.com>
2016-05-30 20:42:25 -0500

    Added My Team Activity & got add team button working

--- app/src/main/java/com/example/android/concept_list/TeamListActivity.java
+++ app/src/main/java/com/example/android/concept_list/TeamListActivity.java
@@ -2,10 +2,13 @@ package com.example.android.concept_list;
 
 import android.support.v4.app.Fragment;
 
-public class MainActivity extends SingleFragmentActivity {
+/**
+ * Created by cholni01 on 5/30/2016.
+ */
+public class TeamListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment(){
-        return new MyTeamListFragment();
+        return new TeamListFragment();
     }
 }
